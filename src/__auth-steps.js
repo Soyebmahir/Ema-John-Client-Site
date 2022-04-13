beb704b37f7647c79300a564d84ecfe2dd89e986
Soyeb <tssoyeb@gmail.com>
2022-04-13 12:07:44 +0600

    created user by react-firebase-hooks

--- src/__auth-steps.js
+++ src/__auth-steps.js
@@ -2,5 +2,10 @@
  * 1.create a new project on console.google.com 
  * 2.npm install firebase
  * 3.create firebase.init.js and import getAuth and export auth variable with app parameter
- * 4
+ * 4. firebase settings> Authentication> enable Email and Password auth
+ * 5. create login signUp component and sset up route
+ * 6.attach form field handler and form submit handler
+ * 7. npm install -- save react-firebase-hooks
+ * 8. useCreateUserWithEmailPassword from reatc-firebase-hooks
+ * 9.if user is created then redirect to the expected field
 */ 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 const SignUp = () => {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [confirmPassword,setConfirmPassword]=useState('')
+    const [error,setError]=useState('')
+    const navigate =useNavigate()
+
+    const [ createUserWithEmailAndPassword,user]=useCreateUserWithEmailAndPassword(auth);
+    if(user){
+        navigate('/shop')
+    }
+
+    const hanndleEmailBlur =event=>{
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur=event=>{
+        setPassword(event.target.value)
+    }
+    const handleConfirmPassword=event=>{
+        setConfirmPassword(event.target.value)
+    }
+    const handleCreateUser=event=>{
+        event.preventDefault()
+        if(password!==confirmPassword){
+            setError('Your two password did not match')
+        }
+        if(password.length <6){
+            setError('Password must contain atleast six character')
+            return;
+        }
+        setError('')
+        createUserWithEmailAndPassword(email,password);
+
+    }
     return (
         <div className='form-container'>
         <div>
         <h1 className='form-title'>Sign Up</h1>
-        <form >
+        <form onSubmit={handleCreateUser} >
             <div className="input-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' />
+                <input onBlur={hanndleEmailBlur} type="email" name='email'  required/>
             </div>
             <div className="input-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" name='password' />
+                <input      onBlur={handlePasswordBlur} type="password" name='password'  required/>
             </div>
             <div className="input-group">
                 <label htmlFor="confirm-password">Confirm Password</label>
-                <input type="password" name='confirm-password' />
+                <input onBlur={handleConfirmPassword} type="password" name='confirm-password'  required/>
             </div>
-            <input className='form-submit' type="submit" value='login' />
+            <p style={{color:'red'}}>{error}</p>
+            <input className='form-submit' type="submit" value='Sign Up' />
 
         </form>
         <p className='create-account'> Already have an account ? <Link className='form-link' to='/login'>Login</Link></p>
