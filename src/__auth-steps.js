a8a20cd97b6ba6781cebca93b75842e8cc17aa6f
Soyeb <tssoyeb@gmail.com>
2022-04-13 09:56:53 +0600

    create login sign Up component

--- src/__auth-steps.js
+++ src/__auth-steps.js
@@ -1,6 +1,6 @@
 /** 
  * 1.create a new project on console.google.com 
  * 2.npm install firebase
- * 3.
+ * 3.create firebase.init.js and import getAuth and export auth variable with app parameter
  * 4
 */ 
